95762ba30c32860a42c3526180a137dfa3853782
e.labruna <e.labruna@soluzionipiu.it>
2025-11-06 14:51:43 +0100

    creazione header e prime card statiche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NgStyle } from "../../node_modules/@angular/common/index";
 
 @Component({
   selector: 'app-root',
