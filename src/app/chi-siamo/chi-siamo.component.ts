a04134f7e59bb95a50ea5a6db2646e47c095b737
e.labruna <e.labruna@soluzionipiu.it>
2025-11-11 11:45:01 +0100

    Aggiunta seconda sezione alla pagine chi siamo

--- src/app/chi-siamo/chi-siamo.component.ts
+++ src/app/chi-siamo/chi-siamo.component.ts
@@ -1,20 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-chi-siamo',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule],
   templateUrl: './chi-siamo.component.html',
   styleUrls: ['./chi-siamo.component.css']
 })
-export class ChiSiamoComponent implements OnInit {
-  attivo = false;
+export class ChiSiamoComponent {
+  attivo = false;          // per la prima sezione
+  mostraSeconda = false;   // per la seconda sezione
 
   ngOnInit(): void {
     setTimeout(() => {
       this.attivo = true;
     }, 300);
+  }
+
+  toggleSeconda(): void {
+    this.mostraSeconda = !this.mostraSeconda;
   }
 }
